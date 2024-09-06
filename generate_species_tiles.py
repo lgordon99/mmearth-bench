918cad0e4363a3c7c9a18dce93bd31edc17b8b5b
Lucia Gordon <luciagordon@g.harvard.edu>
2024-09-06 13:09:26 -0400

    Calculated convex hull area

--- generate_species_tiles.py
+++ generate_species_tiles.py
@@ -4,16 +4,22 @@ Need 100GB? RAM to run this file
 '''
 
 # imports
+from geopy.distance import geodesic
+from pyproj import Geod
+from shapely.geometry import MultiPoint, Point, Polygon
 from sys import argv
 from utils import read_yaml, read_json
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import ee
+import geopandas as gpd
 import json
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import pandas as pd
+import random
 import requests
 
 ee.Initialize(project='mmearth-bench') # initializes EE with our project
@@ -34,33 +40,63 @@ def save_2020_observations():
     print(f'{len(observations_2020)} observations in 2020')
     print(f'{observations_2020["taxon_id"].nunique()} species in 2020')
 
-def get_species_type(species_name):
-    return requests.get(f'https://api.inaturalist.org/v1/taxa?q={species_name}').json()['results'][0]['iconic_taxon_name']
+def get_species_type(species):
+    response = requests.get(f'https://api.inaturalist.org/v1/taxa?q={species}')
 
-def plot_num_observations_per_species():
+    if response.status_code == 200:
+        data = response.json()['results'][0]['iconic_taxon_name']
+        return data
+    else:
+        get_species_type(species)
+
+def calculate_spread(observations):
+    observations = observations.values.tolist()
+    # distances = []
+
+    # for i in range(len(observations)):
+    #     for j in range(len(observations)):
+    #         if i != j:
+    #             distances.append(geodesic(observations[i], observations[j]).kilometers)
+
+    # return np.mean(distances)
+
+
+    # Use Geod to calculate the area of the convex hull on Earth's surface
+    geod = Geod(ellps='WGS84')
+    area, _ = geod.geometry_area_perimeter(MultiPoint(observations).convex_hull)
+
+    return abs(area)
+
+def plot_species_count():
     observations_2020 = pd.read_csv(f'{data_dir_path}/sinr-data/observations_2020.csv', usecols=['latitude', 'longitude', 'taxon_id', 'month']) # reads in the 2020 data
     num_observations = len(observations_2020) # gets the total number of observations
     metadata = read_json(f'{data_dir_path}/sinr-data/train/geo_prior_train_meta.json') # reads in metadata
     species_id_name = {item['taxon_id']: item['latin_name'] for item in metadata} # maps each taxon ID to a species name
     observations_2020['species'] = observations_2020['taxon_id'].map(species_id_name) # creates a species column
-    num_observations_per_species = observations_2020['species'].value_counts().reset_index() # number of observations for each species
-    num_observations_per_species = num_observations_per_species[num_observations_per_species['count'] >= 300] # keeps species with at least 300 observations
-    species_spread = observations_2020.groupby('species')[['latitude', 'longitude']].std().reset_index() # calculates STD of latitude and longitude
-    species_spread['spread'] = (species_spread['latitude'] ** 2 + species_spread['longitude'] ** 2) ** 0.5 # calculates sqrt(lat_std^2 + lon_std^2)
-    num_observations_per_species = pd.merge(num_observations_per_species, species_spread[['species', 'spread']], on='species', how='left') # adds spread column
-    num_observations_per_species = num_observations_per_species.sort_values(by='spread', ascending=False).head(100) # takes the top 100 species in terms of spread
-    num_observations_per_species = num_observations_per_species.sort_values(by='count', ascending=False) # sorts the species in descending order by count
-    species_types = {species: get_species_type(species) for species in num_observations_per_species['species']} 
-    num_observations_per_species['type'] = num_observations_per_species['species'].map(species_types)
-    type_counts = num_observations_per_species['type'].value_counts().reset_index()
+    species_count = observations_2020['species'].value_counts().reset_index() # number of observations for each species
+    species_count = species_count[species_count['count'] >= 300] # keeps species with at least 300 observations
+    species_count_300 = species_count['species'].tolist()
+    observations_species_count_300 = observations_2020[observations_2020['species'].isin(species_count_300)]
+    species_spread = observations_species_count_300.groupby('species')[['longitude', 'latitude']].apply(calculate_spread).reset_index(name='spread')
+    print(species_spread.head())
+    # species_spread = observations_2020.groupby('species')[['latitude', 'longitude']].std().reset_index() # calculates STD of latitude and longitude
+    # species_spread['spread'] = (species_spread['latitude'] ** 2 + species_spread['longitude'] ** 2) ** 0.5 # calculates sqrt(lat_std^2 + lon_std^2)
+    species_count = pd.merge(species_count, species_spread[['species', 'spread']], on='species', how='left') # adds spread column
+    species_count = species_count.sort_values(by='spread', ascending=False).head(100) # takes the top 100 species in terms of spread
+    species_count = species_count.sort_values(by='count', ascending=False) # sorts the species in descending order by count
+    species_types = {species: requests.get(f'https://api.inaturalist.org/v1/taxa?q={species}').json()['results'][0]['iconic_taxon_name'] for species in species_count['species']} 
+    species_count['type'] = species_count['species'].map(species_types) # creates a type column
+    type_counts = species_count['type'].value_counts().reset_index() # counts the occurrences of each type
 
+    # plot count per species
     fig, ax = plt.subplots(dpi=300)
-    ax.bar(num_observations_per_species['species'], num_observations_per_species['count'])
+    ax.bar(species_count['species'], species_count['count'])
     ax.set_ylabel('Count')
     plt.xticks(rotation=90, fontsize=4)
     plt.savefig('figures/count_per_species.pdf', bbox_inches='tight', pad_inches=0.1, transparent=False)
     plt.savefig('figures/count_per_species.png', bbox_inches='tight', pad_inches=0.1, transparent=False)
 
+    # plot count per type
     fig, ax = plt.subplots(dpi=300)
     ax.bar(type_counts['type'], type_counts['count'])
     ax.set_ylabel('Count')
@@ -68,49 +104,77 @@ def plot_num_observations_per_species():
     plt.savefig('figures/count_per_type.pdf', bbox_inches='tight', pad_inches=0.1, transparent=False)
     plt.savefig('figures/count_per_type.png', bbox_inches='tight', pad_inches=0.1, transparent=False)
 
+    selected_species = species_count['species'].tolist() # gets species selected by above filtering procedure
+    observations_selected_species = observations_2020[observations_2020['species'].isin(selected_species)][['species', 'month', 'longitude', 'latitude']].values.tolist() # gets all observations for the selected species
+    observations_dict = {species: [[observation[1], observation[2], observation[3]] for observation in observations_selected_species if observation[0] == species] for species in selected_species} # groups observations by species
+    selected_observations_dict = {species: random.sample(observations_dict[species], 300) for species in selected_species} # samples 300 observations per species
+    observations_selected_species_df = observations_2020[observations_2020['species'].isin(selected_species)][['species', 'month', 'longitude', 'latitude']] # gets all observations for the selected species
+    observations_selected_species_df['geometry'] = observations_selected_species_df.apply(lambda row: Point(row['longitude'], row['latitude']), axis=1) # adds a geometry column
+    observations_gdf = gpd.GeoDataFrame(observations_selected_species_df, geometry='geometry') # creates a GeoDataFrame for all the observations for the selected species
+
+    print(np.array(list(selected_observations_dict.values())).shape)
+
+    TILE_SIZE = read_yaml('config.yml')['TILE_SIZE']
+    tiles = np.concatenate([ee.FeatureCollection([ee.Feature(ee.Geometry.Point([observation[1], observation[2]]).buffer(TILE_SIZE / 2).bounds()).set({'species': species, 'month': observation[0]}) for observation in selected_observations_dict[species]]).getInfo()['features'] for species in selected_species])
+    tiles = [{**{key: value for key, value in tile.items() if key != 'id'}} for tile in tiles]
+
+    for tile in tiles:
+        month = tile['properties']['month']
+        previous_month = month - 1 if month > 1 else 12
+        next_month = month + 1 if month < 12 else 1
+        observations_in_tile = observations_gdf[observations_gdf.within(Polygon(tile['geometry']['coordinates'][0]))]
+        filtered_data = observations_in_tile[(observations_in_tile['month'] >= previous_month) & (observations_in_tile['month'] <= next_month)]
+        tile['properties']['species'] = list(filtered_data['species'].unique())
+
+    print(f'{len(tiles)} tiles')
+    os.makedirs('tiles/species', exist_ok=True)
+
+    geojson_collection = {'type': 'FeatureCollection', 'features': tiles}
+
+    with open('tiles/species/species_tiles.geojson', 'w') as f:
+        json.dump(geojson_collection, f, indent=4) # save the tiles as a GeoJSON
+
+    tiles_list = read_geojson('tiles/species/species_tiles.geojson')['features']
+    print(len(tiles_list))
+
     fig = plt.figure(dpi=300)
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.add_feature(cfeature.BORDERS, linestyle='-', linewidth=0.5)
     ax.add_feature(cfeature.COASTLINE)
 
-    selected_species = num_observations_per_species['species'].unique() # gets species selected by above filtering procedure
-    observations_selected_species = observations_2020[observations_2020['species'].isin(selected_species)][['longitude', 'latitude', 'species']].values.tolist() # gets all observations for the selected species
-    observations_dict = {species: [[observation[0], observation[1]] for observation in observations_selected_species if observation[2] == species] for species in selected_species}
+    selected_observations = np.array(list(selected_observations_dict.values()))[:, :, 1:].reshape(-1, 2)
 
-    for lon, lat, _ in observations_selected_species:
-        plt.plot(lon, lat, marker='o', color='red', markersize=1, transform=ccrs.PlateCarree())
+    for observation in selected_observations:
+        lon, lat = observation
+        plt.plot(lon, lat, marker='o', color='red', markersize=0.5, transform=ccrs.PlateCarree())
 
     ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
     plt.savefig('map.pdf', bbox_inches='tight')
     plt.savefig('map.png', bbox_inches='tight')
 
-    print(f'{num_observations} observations')
-    print(f'{len(num_observations_per_species)} species')
-    print(len(species_spread))
-    print(num_observations_per_species.columns.tolist())
-    print(num_observations_per_species.head())
+    # print(f'{num_observations} observations')
+    # print(f'{len(species_count)} species')
+    # print(len(species_spread))
+    # print(species_count.columns.tolist())
+    # print(species_count.head())
 
-def generate_species_tiles():
-    TILE_SIZE = read_yaml('config.yml')['TILE_SIZE']
-    tile = ee.Feature(ee.Geometry.Point([lon, lat])).map(lambda point: point.buffer(TILE_SIZE / 2).bounds())
-    tiles = [{**{key: value for key, value in tile.items() if key != 'id'}, 'properties': {'biome': biome, 'ecoregion': ecoregion}} for tile in tiles.getInfo()['features']]
-    # TODO: get coordinates from observations_dict to make tiles
+# def generate_species_tiles():
 # ecoregions_collection = ee.FeatureCollection('RESOLVE/ECOREGIONS/2017')
 # title = 'biomes_ecoregions.json'
 
 # species = observations_2020['taxon_id'].unique()
-# num_observations_per_species = observations_2020
+# species_count = observations_2020
 
-# num_observations_per_species = {species_: len(observations_2020[observations_2020['taxon_id'] == species_]) for species_ in species}
-# num_observations_per_species = dict(sorted(num_observations_per_species.items(), key=lambda item: item[1], reverse=True))
+# species_count = {species_: len(observations_2020[observations_2020['taxon_id'] == species_]) for species_ in species}
+# species_count = dict(sorted(species_count.items(), key=lambda item: item[1], reverse=True))
 
-# print(num_observations_per_species[:10])
+# print(species_count[:10])
 
 # fig, ax = plt.subplots(dpi=300)
-# ax.bar(species, num_observations_per_species.values())
+# ax.bar(species, species_count.values())
 # ax.set_ylabel('# Observations')
-# plt.savefig('figures/num_observations_per_species.pdf', bbox_inches='tight', pad_inches=0.1, transparent=False)
-# plt.savefig('figures/num_observations_per_species_results.png', bbox_inches='tight', pad_inches=0.1, transparent=False)
+# plt.savefig('figures/species_count.pdf', bbox_inches='tight', pad_inches=0.1, transparent=False)
+# plt.savefig('figures/species_count_results.png', bbox_inches='tight', pad_inches=0.1, transparent=False)
 
 # species_observation_collection = ee.FeatureCollection([ee.Feature(ee.Geometry.Point([observation.longitude, observation.latitude]), {'species': observation.taxon_id, 'month': observation.month}) for observation in observations_2020.itertuples()])
 
@@ -203,5 +267,5 @@ if __name__ == '__main__':
     if not os.path.exists(f'{data_dir_path}/sinr-data/observations_2020.csv'):
         save_2020_observations()
 
-    plot_num_observations_per_species()
+    plot_species_count()
     # generate_species_tiles()
