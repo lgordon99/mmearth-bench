ffa1385c2f20f1f688888017de09f255e7b510d6
Lucia Gordon <luciagordon@g.harvard.edu>
2024-09-09 14:42:36 -0400

    Removed old code

--- generate_species_tiles.py
+++ generate_species_tiles.py
@@ -59,7 +59,7 @@ def calculate_spread(observations):
 
     return abs(area)
 
-def plot_species_count():
+def generate_species_tiles():
     observations_2020 = pd.read_csv(f'{data_dir_path}/sinr-data/observations_2020.csv', usecols=['latitude', 'longitude', 'taxon_id', 'month']) # reads in the 2020 data
     num_observations = len(observations_2020) # gets the total number of observations
     metadata = read_json(f'{data_dir_path}/sinr-data/train/geo_prior_train_meta.json') # reads in metadata
@@ -164,120 +164,8 @@ def plot_species_count():
     plt.savefig('map.pdf', bbox_inches='tight')
     plt.savefig('map.png', bbox_inches='tight')
 
-    # print(f'{num_observations} observations')
-    # print(f'{len(species_count)} species')
-    # print(len(species_spread))
-    # print(species_count.columns.tolist())
-    # print(species_count.head())
-
-# def generate_species_tiles():
-# ecoregions_collection = ee.FeatureCollection('RESOLVE/ECOREGIONS/2017')
-# title = 'biomes_ecoregions.json'
-
-# species = observations_2020['taxon_id'].unique()
-# species_count = observations_2020
-
-# species_count = {species_: len(observations_2020[observations_2020['taxon_id'] == species_]) for species_ in species}
-# species_count = dict(sorted(species_count.items(), key=lambda item: item[1], reverse=True))
-
-# print(species_count[:10])
-
-# fig, ax = plt.subplots(dpi=300)
-# ax.bar(species, species_count.values())
-# ax.set_ylabel('# Observations')
-# plt.savefig('figures/species_count.pdf', bbox_inches='tight', pad_inches=0.1, transparent=False)
-# plt.savefig('figures/species_count_results.png', bbox_inches='tight', pad_inches=0.1, transparent=False)
-
-# species_observation_collection = ee.FeatureCollection([ee.Feature(ee.Geometry.Point([observation.longitude, observation.latitude]), {'species': observation.taxon_id, 'month': observation.month}) for observation in observations_2020.itertuples()])
-
-# def make_species_observation_collection():
-#     df = pd.read_csv('/n/tambe_lab/Users/luciagordon/sinr-data/train/geo_prior_train.csv')
-#     df['year'] = df['observed_on'].str.split('-').str[0].astype(int)
-#     df['month'] = df['observed_on'].str.split('-').str[1].astype(int)
-
-#     print(f'Columns = {df.columns.tolist()}')
-#     print(f'{len(df)} observations')
-#     print(f'{df["taxon_id"].nunique()} species')
-
-#     observations_post_2017 = df[df['year'] >= 2017]
-
-#     print(f'{len(observations_post_2017)} observations between 2017-2021')
-#     print(f'{observations_post_2017["taxon_id"].nunique()} species between 2017-2021')
-
-#     observations_2020 = df[df['year'] == 2020]
-
-#     print(f'{len(observations_2020)} observations in 2020')
-#     print(f'{observations_2020["taxon_id"].nunique()} species in 2020')
-
-#     species_observation_collection = ee.FeatureCollection([ee.Feature(ee.Geometry.Point([observation.longitude, observation.latitude]), {'species': observation.taxon_id, 'month': observation.month}) for observation in observations_2020.itertuples()])
-
-#     with open('species_observation_collection.geojson', 'w') as file:
-#         json.dump(species_observation_collection.getInfo(), file, indent=4)
-
-def generate_species_tiles():
-    NUM_TILES = 1000
-
-    if not os.path.exists(f'biomes_ecoregions_data/{title}'):
-        get_biomes_ecoregions_area(ecoregions_collection=ecoregions_collection, title=title)
-
-    biomes_ecoregions = read_json(f'biomes_ecoregions_data/{title}')
-    biomes = biomes_ecoregions.keys()
-    num_tiles_per_biome = math.ceil(NUM_TILES / len(biomes))
-    tiles = []
-    os.makedirs('tiles', exist_ok=True)
-
-    print(f'{len(biomes)} biomes')
-    print(f'{num_tiles_per_biome} tiles per biome')
-
-    for i, biome in enumerate(biomes):
-        print(f'Biome {i+1}/{len(biomes)}: {biome}')
-
-        ecoregions = biomes_ecoregions[biome]['ecoregions'].keys()
-
-        for j, ecoregion in enumerate(ecoregions):
-            print(f'Ecoregion {j+1}/{len(ecoregions)}: {ecoregion}')
-
-            ecoregion_collection = ecoregions_collection.filter(ee.Filter.eq('ECO_NAME', ecoregion))
-            # ecoregion_points = species_observation_collection.filterBounds(ecoregion_collection)
-            # num_ecoregion_tiles = min(ecoregion_points.size().getInfo(), math.ceil(num_tiles_per_biome * biomes_ecoregions[biome]['ecoregions'][ecoregion]['area'] / biomes_ecoregions[biome]['area'])) # number of tiles per ecoregion is proportional to the size of the ecoregion
-            num_ecoregion_tiles = math.ceil(num_tiles_per_biome * biomes_ecoregions[biome]['ecoregions'][ecoregion]['area'] / biomes_ecoregions[biome]['area']) # number of tiles per ecoregion is proportional to the size of the ecoregion
-
-            print(f'{num_ecoregion_tiles} tiles in ecoregion')
-            TILE_SIZE = read_yaml('config.yml')['TILE_SIZE']
-            tiles = (species_observation_collection
-                     .filterBounds(ecoregion_collection)
-                     .map(lambda point: ee.Feature(point.geometry())) # only keeps geometry information
-                     .randomColumn('random') # add a new property to each feature containing a random number
-                     .sort('random') # sort the features by the random numbers
-                     .limit(num_ecoregion_tiles) # select the first points
-                     .map(lambda point: point.buffer(TILE_SIZE / 2).bounds())) # convert points to tiles
-            tiles = [{**{key: value for key, value in tile.items() if key != 'id'}, 'properties': {'biome': biome, 'ecoregion': ecoregion}} for tile in tiles.getInfo()['features']]
-            print(f'{len(tiles)} tiles made')
-
-            # while num_ecoregion_tiles > 0:
-            #     candidate_tile = ee.FeatureCollection.randomPoints(region=ecoregion_points, points=1).map(lambda point: point.buffer(TILE_SIZE / 2).bounds()).first().getInfo()
-
-            #     # for tile in tiles:
-            #     #     if ee.Geometry.Polygon(candidate_tile['geometry']['coordinates']).intersects(ee.Geometry.Polygon(tile['geometry']['coordinates']), maxError=1).getInfo(): # if the candidate tile overlaps with any existing tiles
-            #     #         print('Overlap detected')
-            #     #         continue
-
-            #     candidate_tile['id'] = len(tiles)
-            #     candidate_tile['properties'] = {'biome': biome, 'ecoregion': ecoregion}
-            #     print(candidate_tile)
-            #     tiles.append(candidate_tile)
-            #     num_ecoregion_tiles -= 1
-            #     print(f'{len(tiles)} tiles total')
-
-            #     random.shuffle(tiles) # shuffling the tiles list
-            geojson_collection = {'type': 'FeatureCollection', 'features': tiles}
-
-            with open(f'tiles/species/species_tiles/biome_{biome}_ecoregion_{ecoregion.replace("/", "_")}_biomass_tiles.geojson', 'w') as f:
-                json.dump(geojson_collection, f, indent=4)
-
 if __name__ == '__main__':
     if not os.path.exists(f'{data_dir_path}/sinr-data/observations_2020.csv'):
         save_2020_observations()
 
-    plot_species_count()
-    # generate_species_tiles()
+    generate_species_tiles()
