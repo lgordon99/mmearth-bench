7e1c761605b1859fc50a307cddf25315ce11a5a4
Lucia Gordon <luciagordon@g.harvard.edu>
2024-11-24 23:08:10 -0500

    Saved image-level modalities to tiff

--- ee_data.py
+++ ee_data.py
@@ -14,6 +14,7 @@ import math
 import numpy as np
 import os
 import pandas as pd
+import rasterio
 import requests
 import shutil
 import time
@@ -37,10 +38,8 @@ class EEData:
                                      'lat_cos': np.cos(np.deg2rad(self.lat)),
                                      'lon_sin': np.sin(np.deg2rad(self.lon)),
                                      'lon_cos': np.cos(np.deg2rad(self.lon))}
-        # self.biome = tile['properties']['biome'] if 'biome' in list(tile['properties'].keys()) else ee.FeatureCollection('RESOLVE/ECOREGIONS/2017').filterBounds(self.polygon).getInfo()['features'][0]['properties']['BIOME_NAME']
-        # self.ecoregion = tile['properties']['ecoregion'] if 'ecoregion' in list(tile['properties'].keys()) else ee.FeatureCollection('RESOLVE/ECOREGIONS/2017').filterBounds(self.polygon).getInfo()['features'][0]['properties']['ECO_NAME']
         self.task = task
-        self.image_set = {}
+        self.modality_data = {}
         self.no_data = False
         self.era5_data = {}
         self.proj = ''
@@ -61,24 +60,24 @@ class EEData:
 
         for function_name in datasets: # series of function calls to get the data
             if getattr(self, function_name)() is False: # if the method returns False
-                print(f'{function_name} returned False')
-                print(f'Skipping tile with ID {self.id}')
                 self.no_data = True
+                print(f'{function_name} returned False')
+
                 break
 
         if task == 'biomass':
             self.biomass()
 
         if not self.no_data:
-            merged_image = self.image_set[datasets[0]] # start with Sentinel-2
+            merged_image = self.modality_data[datasets[0]] # start with Sentinel-2
 
-            for dataset, value in self.image_set.items():
+            for dataset, value in self.modality_data.items():
                 if dataset == datasets[0]:
                     continue # already included Sentinel-2
                 else:
                     merged_image = ee.Image.cat([merged_image, value])
 
-            self.export_local_single(merged_image)
+            self.save_tiff(merged_image)
 
             print(f'Time elapsed for this tile: {round(time.time() - start, 2)}s')
 
@@ -90,15 +89,13 @@ class EEData:
                               .filter(ee.Filter.contains('.geo', self.polygon.buffer(200))) # gets images containing the tile plus some buffer
                               .map(lambda image: image.clip(self.polygon))) # crops to tile
 
-        print(f'{sentinel2_images.size().getInfo()} Sentinel-2 images returned')
+        if sentinel2_images.size().getInfo() == 0: # if no images were returned
+            return False
 
         msk_cldprob_res = sentinel2_images.select('MSK_CLDPRB').first().projection().nominalScale().getInfo() # resolution of MSK_CLDPRB band
-        cloudy_pixel_frac_list = ((sentinel2_images
-                                   .select('MSK_CLDPRB') # select MSK_CLDPRB band
-                                   .map(lambda image: image.set('cloudy_pixel_frac', image.gte(0.1).reduceRegion(reducer=ee.Reducer.mean(), scale=msk_cldprob_res)))) # pixels >= 0.1 cloud probability are cloudy
-                                  .aggregate_array('cloudy_pixel_frac').getInfo()) # get the cloudy pixel fraction for each image
-        cloudy_pixel_fracs = [list(item.values())[0] for item in cloudy_pixel_frac_list] # extract just the cloudy pixel fraction value
-        least_cloudy_image_index = int(np.argmin(cloudy_pixel_fracs)) # get the index for an image with the fewest cloudy pixels
+        sentinel2_images = sentinel2_images.map(lambda image: image.set('cloudy_pixel_frac', image.select('MSK_CLDPRB').gte(0.1).reduceRegion(reducer=ee.Reducer.mean(), scale=msk_cldprob_res).get('MSK_CLDPRB'))).filter(ee.Filter.notNull(['cloudy_pixel_frac'])) # pixels >= 0.1 cloud probability are cloudy    
+        cloudy_pixel_fracs = (sentinel2_images.aggregate_array('cloudy_pixel_frac').getInfo()) # gets the cloudy pixel fraction for each image
+        least_cloudy_image_index = int(np.argmin(cloudy_pixel_fracs)) # gets the index for an image with the fewest cloudy pixels
 
         if cloudy_pixel_fracs[least_cloudy_image_index] >= 0.1: # if the least cloudy image has > 10% cloudy pixels
             return False # skipping tile
@@ -106,10 +103,11 @@ class EEData:
         s2_image = ee.Image(sentinel2_images.toList(sentinel2_images.size()).get(least_cloudy_image_index)).float() # get the least cloudy S2 image
         self.s2_date = s2_image.date().format('YYYY-MM-dd').getInfo() # date of S2 image
         month = int(self.s2_date.split('-')[1])
-        self.month_encoding = {'sin': np.sin(np.pi * month / 6), 'cos': np.cos(np.pi * month / 6)}
+        self.month_encoding = {'month_sin': np.sin(np.pi * month / 6), 'month_cos': np.cos(np.pi * month / 6)}
         self.proj = s2_image.select('B4').projection() # projection of B4 band
         self.crs = self.proj.getInfo()['crs'] # CRS of B4 band
-        self.image_set['sentinel2'] = s2_image.select([band for band in bands if band not in ['SCL', 'QA60']]).resample('bilinear').reproject(self.proj).addBands(s2_image.select(['SCL', 'QA60']).reproject(self.proj))
+        s2_image = s2_image.select([band for band in bands if band not in ['SCL', 'QA60']]).resample('bilinear').reproject(self.proj).addBands(s2_image.select(['SCL', 'QA60']).reproject(self.proj))
+        self.modality_data['sentinel2'] = s2_image.rename([f'Sentinel2_{band}' for band in bands])
 
     def sentinel1(self):
         bands = ['VV', 'VH', 'HH', 'HV']
@@ -134,27 +132,27 @@ class EEData:
         # adding ascending bands
         for band in bands:
             if band in asc_image.bandNames().getInfo():
-                band_data = asc_image.select(band).float().resample('bilinear').reproject(self.proj).rename(f'asc_{band}')
+                band_data = asc_image.select(band).float().resample('bilinear').reproject(self.proj).rename(f'Sentinel1_ascending_{band}')
             else:
-                band_data = nan_band.rename(f'asc_{band}')
+                band_data = nan_band.rename(f'Sentinel1_ascending_{band}')
 
             s1_image = band_data if s1_image is None else ee.Image.cat([s1_image, band_data])
 
         # adding descending bands
         for band in bands:
             if band in desc_image.bandNames().getInfo():
-                band_data = desc_image.select(band).float().resample('bilinear').reproject(self.proj).rename(f'desc_{band}')
+                band_data = desc_image.select(band).float().resample('bilinear').reproject(self.proj).rename(f'Sentinel1_descending_{band}')
             else:
-                band_data = nan_band.rename(f'desc_{band}')
+                band_data = nan_band.rename(f'Sentinel1_descending_{band}')
 
             s1_image = band_data if s1_image is None else ee.Image.cat([s1_image, band_data])
 
-        self.image_set['sentinel1'] = s1_image
+        self.modality_data['sentinel1'] = s1_image
 
     def aster(self):
         elevation = ee.Image('projects/sat-io/open-datasets/ASTER/GDEM').clip(self.polygon).select('b1').float() # get elevation band
         slope = ee.Terrain.slope(elevation) # calculate slope from elevation data
-        self.image_set['aster'] = ee.Image.cat([elevation, slope]).resample('bilinear').reproject(self.proj) # combine the elevation and slope into a single image
+        self.modality_data['aster'] = ee.Image.cat([elevation, slope]).resample('bilinear').reproject(self.proj).rename(['AsterDEM_elevation', 'AsterDEM_slope']) # combine the elevation and slope into a single image
 
     def dynamic_world(self):
         '''
@@ -197,7 +195,7 @@ class EEData:
                     .where(image.eq(7), 8)\
                     .where(image.eq(8), 9)\
                     .where(image.eq(9), 10)
-            
+
             # replacing the label band with the new label band
             image = image.addBands(label2.rename('label2'))
             image = image.select('label2')
@@ -206,14 +204,13 @@ class EEData:
 
         dynamic_world_images = dynamic_world_images.map(reclasify)
         dw_image = dynamic_world_images.mode().clip(self.polygon)
-        # print(dw_image.sampleRectangle(region=self.polygon).get('label2').getInfo())
         bands = dw_image.bandNames().getInfo()
 
         if len(bands) == 0:
-            self.image_set['dynamic_world'] = None
+            self.modality_data['dynamic_world'] = None
         else:
             dw_image = dw_image.reproject(self.proj)
-            self.image_set['dynamic_world'] = dw_image
+            self.modality_data['dynamic_world'] = dw_image.rename('DynamicWorld')
 
     def canopy_height_eth(self):
         '''
@@ -222,66 +219,99 @@ class EEData:
 
         height = ee.Image('users/nlang/ETH_GlobalCanopyHeight_2020_10m_v1').clip(self.polygon).float()
         std = ee.Image('users/nlang/ETH_GlobalCanopyHeightSD_2020_10m_v1').clip(self.polygon).float()
-        self.image_set['canopy_height_eth'] = ee.Image.cat([height, std]).resample('bilinear').reproject(self.proj).rename(['height', 'std'])
+        self.modality_data['canopy_height_eth'] = ee.Image.cat([height, std]).resample('bilinear').reproject(self.proj).rename(['ETHGCH_canopy_height', 'ETHGCH_canopy_height_uncertainty'])
 
     def esa_worldcover(self):
         ''' Gets the ESA worldcover data '''
 
-        self.image_set['esa_worldcover'] = ee.ImageCollection('ESA/WorldCover/v100').first().clip(self.polygon).select('Map').reproject(self.proj)
+        self.modality_data['esa_worldcover'] = ee.ImageCollection('ESA/WorldCover/v100').first().clip(self.polygon).select('Map').reproject(self.proj).rename('ESA_Worldcover')
 
     def era5(self):
         collection = 'ECMWF/ERA5_LAND/MONTHLY_AGGR'
         bands = ['temperature_2m', 'temperature_2m_min', 'temperature_2m_max', 'total_precipitation_sum']
         year, month, _ = list(map(int, self.s2_date.split('-')))
-        last_day_of_current_month = ((datetime(year, month, 1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)).strftime('%Y-%m-%d')
-        first_day_current_month_prev_year = datetime(year-1, month, 1).strftime('%Y-%m-%d')
-        first_day_prev_month = f'{year}-{month-1}-01' if month > 1 else f'{year-1}-12-01'
-        era5_monthly = (ee.ImageCollection(collection)
-                          .filterDate(first_day_prev_month, last_day_of_current_month) # previous month and current month
+        month_first_day = datetime(year, month, 1)
+        month_last_day = ((month_first_day + timedelta(days=32)).replace(day=1) - timedelta(days=1)).strftime('%Y-%m-%d')
+        last_month_first_day = datetime(year, month-1, 1)
+        last_month_last_day = ((last_month_first_day + timedelta(days=32)).replace(day=1) - timedelta(days=1)).strftime('%Y-%m-%d')
+        era5_month = (ee.ImageCollection(collection)
+                          .filterDate(month_first_day, month_last_day)
                           .map(lambda image: image.clip(self.polygon))
                           .select(bands)
                           .toBands()
-                          .rename([f'this_month_{band}' if i < 4 else f'last_month_{band}' for i, band in enumerate(2 * bands)]))
-        era5_yearly = (ee.ImageCollection(collection)
-                         .filterDate(first_day_current_month_prev_year, last_day_of_current_month)
+                          .rename(['temperature_month_mean', 'temperature_month_min', 'temperature_month_max', 'precipitation_month']))
+        era5_last_month = (ee.ImageCollection(collection)
+                          .filterDate(last_month_first_day, last_month_last_day)
+                          .map(lambda image: image.clip(self.polygon))
+                          .select(bands)
+                          .toBands()
+                          .rename(['temperature_last_month_mean', 'temperature_last_month_min', 'temperature_last_month_max', 'precipitation_last_month']))
+        last_year_month_first_day = datetime(year-1, month, 1).strftime('%Y-%m-%d')
+        era5_year = (ee.ImageCollection(collection)
+                         .filterDate(last_year_month_first_day, month_last_day)
                          .map(lambda image: image.clip(self.polygon)))
+        band_reducer = {'temperature_2m':  ee.Reducer.mean(),
+                        'temperature_2m_min': ee.Reducer.min(),
+                        'temperature_2m_max': ee.Reducer.max(),
+                        'total_precipitation_sum': ee.Reducer.sum()}
+        era5_year = ee.ImageCollection([era5_year.select(band).reduce(band_reducer[band]) for band in bands]).toBands().rename(['temperature_year_mean', 'temperature_year_min', 'temperature_year_max', 'precipitation_year']).float()
+        era5 = ee.Image.cat([era5_month, era5_last_month, era5_year]).reduceRegion(reducer=ee.Reducer.mean(), geometry=self.polygon, scale=10).getInfo()
 
-        def compute_yearly_stat(band):
-            if band == 'temperature_2m':
-                reducer = ee.Reducer.mean()
-            elif band == 'temperature_2m_min':
-                reducer = ee.Reducer.min()
-            elif band == 'temperature_2m_max':
-                reducer = ee.Reducer.max()
-            elif band == 'total_precipitation_sum':
-                reducer = ee.Reducer.sum()
-
-            return era5_yearly.select(band).reduce(reducer)
-
-        era5_yearly_image = ee.ImageCollection([compute_yearly_stat(band) for band in bands]).toBands().rename([f'yearly_{band}' for band in bands]).float()
-        era5_image = ee.Image.cat([era5_monthly, era5_yearly_image])
-        era5_image_bands = era5_image.bandNames().getInfo()
-        era5_image = era5_image.reduceRegion(reducer=ee.Reducer.mean(), geometry=self.polygon, scale=10).getInfo()
-
-        if all(item is None for item in list(era5_image.values())):
+        if all(item is None for item in list(era5.values())):
             return False
 
-        self.era5_data = {band: era5_image[band] for band in era5_image_bands}
+        self.era5_data = era5
 
     def biomass(self):
         '''Gets GEDI aboveground biomass data'''
 
         image = ee.Image.constant(-9999).paint(self.biomass_points, 'agbd').reproject(self.proj) # image array with -9999 wherever there are no points, aligned with Sentinel-2
-        self.image_set['biomass'] = image
+        self.modality_data['biomass'] = image
 
-    def export_local_single(self, image):
+    def save_tiff(self, image):
+        band_names = image.bandNames().getInfo()
         url = image.getDownloadUrl({'name': f'tile_{self.id}',
                                     'scale': 10,
                                     'crs': self.crs,
                                     'region': self.polygon.getInfo()['coordinates'],
                                     'format': 'GeoTIFF',
-                                    'bands': image.bandNames().getInfo()})
+                                    'bands': band_names})
         response = requests.get(url, stream=True, verify=certifi.where())
+        tiff_path = f'{self.task}/pixel_level_data/tile_{self.id}_pixel_level_data.tif'
 
-        with open(f'tiles/{self.task}/pixel_level_data/tile_{self.id}_pixel_level_data.tif', 'wb') as f:
-            f.write(response.content)
+        with open(tiff_path, 'wb') as tiff: # writes in binary mode
+            tiff.write(response.content)
+
+        with rasterio.open(tiff_path) as tiff:
+            tags = tiff.tags()
+            tags.update({'climate_temperature_month_mean': self.era5_data['temperature_month_mean'],
+                         'climate_temperature_last_month_mean': self.era5_data['temperature_last_month_mean'],
+                         'climate_temperature_year_mean': self.era5_data['temperature_year_mean'],
+                         'climate_temperature_month_max': self.era5_data['temperature_month_max'],
+                         'climate_temperature_last_month_max': self.era5_data['temperature_last_month_max'],
+                         'climate_temperature_year_max': self.era5_data['temperature_year_max'],
+                         'climate_temperature_month_min': self.era5_data['temperature_month_min'],
+                         'climate_temperature_last_month_min': self.era5_data['temperature_last_month_min'],
+                         'climate_temperature_year_min': self.era5_data['temperature_year_min'],
+                         'climate_precipitation_month': self.era5_data['precipitation_month'],
+                         'climate_precipitation_last_month': self.era5_data['precipitation_last_month'],
+                         'climate_precipitation_year': self.era5_data['precipitation_year'],
+                         'latitude_sin': self.geolocation_encoding['lat_sin'],
+                         'latitude_cos': self.geolocation_encoding['lat_cos'],
+                         'longitude_sin': self.geolocation_encoding['lon_sin'],
+                         'longitude_cos': self.geolocation_encoding['lon_cos'],
+                         'month_sin': self.month_encoding['month_sin'],
+                         'month_cos': self.month_encoding['month_cos'],
+                         'biome': self.biome,
+                         'ecoregion': self.ecoregion,
+                         'crs': self.crs,
+                         'lat': self.lat,
+                         'lon': self.lon,
+                         's2_date': self.s2_date})
+
+            with rasterio.open(tiff_path, 'w', **tiff.meta) as updated_tiff:
+                updated_tiff.write(tiff.read())
+                updated_tiff.update_tags(**tags)
+
+                for i in range(tiff.count):
+                    updated_tiff.update_tags(i+1, BAND_NAME=band_names[i])
