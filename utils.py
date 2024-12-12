473b41e20395bd6492d96fb1b02b59857e8eb38c
Lucia Gordon <luciagordon@g.harvard.edu>
2024-12-11 19:14:22 -0500

    Modified overlap function to work with points and outer tiles and added the check asset function

--- utils.py
+++ utils.py
@@ -2,12 +2,15 @@
 from shapely.geometry import Polygon
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import ee
 import geojson
 import geopandas as gpd
 import json
 import matplotlib.pyplot as plt
 import subprocess
 import yaml
+
+ee.Initialize(project='mmearth-bench') # initializes EE with our project
 
 def count_running_jobs():    
     result = subprocess.run(['squeue', '--noheader', '--format=%u'], stdout=subprocess.PIPE, text=True) # gets running jobs
@@ -17,10 +20,33 @@ def count_running_jobs():
     return job_count
 
 def format_time(seconds):
-    return f'{int(seconds // 60)} minute(s) {int(seconds % 60)} second(s)'
+    days = int(seconds // 86400)  # number of seconds in a day
+    seconds %= 86400
+    hours = int(seconds // 3600)  # number of seconds in an hour
+    seconds %= 3600
+    minutes = int(seconds // 60)  # number of seconds in a minute
+    seconds %= 60
+    seconds = int(seconds)
 
-def get_last_day_of_month(month):
-    return (datetime(int(year), month, 1) + relativedelta(months=1, days=-1)).day
+    if days > 0:
+        return f'{days} day(s), {hours} hour(s), {minutes} minute(s), {seconds} second(s)'
+    if hours > 0:
+        return f'{hours} hour(s), {minutes} minute(s), {seconds} second(s)'
+    if minutes > 0:
+        return f'{minutes} minute(s), {seconds} second(s)'
+
+def get_asset_if_valid(asset):
+    try:
+        asset.getInfo()
+
+        return asset
+    except ee.ee_exception.EEException as e:
+        if 'not found' in str(e):
+            print(e)
+
+            return None
+        else: # for any other kind of error
+            return asset
 
 def get_rectangle_center(coords):
     x_coords = [coord[0] for coord in coords]
@@ -61,8 +87,8 @@ def read_yaml(path):
         return yaml.safe_load(yaml_file)
 
 def remove_overlapping_tiles(tiles):
-    tiles = [{**{key: value for key, value in tile.items() if key != 'geometry'}, 'geometry': Polygon(tile['geometry']['coordinates'][0])} for tile in tiles]
-    tiles_gdf = gpd.GeoDataFrame(tiles, geometry='geometry').reset_index(drop=False)
+    tiles_for_gdf = [{**{key: value for key, value in tile.items() if key != 'geometry'}, 'geometry': Polygon(tile['properties']['outer_tile']['coordinates'][0])} for tile in tiles]
+    tiles_gdf = gpd.GeoDataFrame(tiles_for_gdf, geometry='geometry').reset_index(drop=False)
     intersections = gpd.sjoin(tiles_gdf, tiles_gdf)
     intersecting_indices = intersections[intersections['index_left'] != intersections['index_right']][['index_left', 'index_right']].to_numpy().tolist()
     indices_to_remove = []
@@ -87,7 +113,8 @@ def save_geojson(features, path):
         print(f'{len(features)} items saved')
 
 def str_to_bool(string):
-    '''convert a string input to a Boolean variable'''
+    '''Convert a string input to a Boolean variable'''
+
     if string.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
 
