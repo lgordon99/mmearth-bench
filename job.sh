54ad120d402c74c29d86ad88a353f08f98e5b786
Lucia Gordon <luciagordon@g.harvard.edu>
2025-02-03 07:56:08 -0500

    Removed memory requirement

--- job.sh
+++ job.sh
@@ -2,7 +2,6 @@
 
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH --mem 500          # Memory pool for all cores (see also --mem-per-cpu) MBs
 date
 source ~/.bashrc
 conda activate $1
