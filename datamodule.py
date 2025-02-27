aa6bcc6a505c8bb66afe37036462232c07d29429
Lucia Gordon <luciagordon@g.harvard.edu>
2025-02-27 08:42:00 -0500

    Plotted train, val, test value distributions on a shared graph

--- datamodule.py
+++ datamodule.py
@@ -26,15 +26,18 @@ class DataModule(LightningDataModule):
     def setup(self, stage):
         self.dataset = self.dataset_class(task=self.task, split_type=self.split_type)
         split_data = utils.read_json(f'{self.task}/{self.task}_{self.split_type}_split_data.json')
-
-        if stage == 'fit':
-            self.train_dataset = Subset(dataset=self.dataset, indices=split_data['train_ids'])
-            self.val_dataset = Subset(dataset=self.dataset, indices=split_data['val_ids'])
-            self._plot_task_distribution('train')
-            self._plot_task_distribution('val')
-        elif stage == 'test':
-            self.test_dataset = Subset(dataset=self.dataset, indices=split_data['test_ids'])
-            self._plot_task_distribution('test')
+        self.train_dataset = Subset(dataset=self.dataset, indices=split_data['train_ids'])
+        self.val_dataset = Subset(dataset=self.dataset, indices=split_data['val_ids'])
+        self.test_dataset = Subset(dataset=self.dataset, indices=split_data['test_ids'])
+        self._plot_task_distribution()
+        # if stage == 'fit':
+        #     self.train_dataset = Subset(dataset=self.dataset, indices=split_data['train_ids'])
+        #     self.val_dataset = Subset(dataset=self.dataset, indices=split_data['val_ids'])
+        #     self._plot_task_distribution('train')
+        #     self._plot_task_distribution('val')
+        # elif stage == 'test':
+        #     self.test_dataset = Subset(dataset=self.dataset, indices=split_data['test_ids'])
+        #     self._plot_task_distribution('test')
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, generator=torch.Generator().manual_seed(42), pin_memory=True)
@@ -48,16 +51,47 @@ class DataModule(LightningDataModule):
     def predict_dataloader(self):
         return DataLoader(self.dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True)
 
-    def _plot_task_distribution(self, stage):
-        dataset = getattr(self, f'{stage}_dataset')
-        task_values = [task_value.squeeze() for _, task_value in dataset]
-        max_value = np.max(task_values)
-        bins = np.arange(0, max_value + 5, 5)
-        plt.hist(task_values, bins=bins)
-        task_name = self.task.replace("_", " ").capitalize()
-        plt.xlabel(f'{task_name} value')
-        plt.ylabel('Count')
-        plt.title(f'{task_name}: Distribution of Task Values in {stage.capitalize()} Dataset')
+    def _plot_task_distribution(self):
+        # for stage in ['train', 'val', 'test']:
+        #     dataset = getattr(self, f'{stage}_dataset')
+        #     task_values = [task_value.squeeze() for _, task_value in dataset]
+
+        #     if stage == 'train':
+        #         print(f'Mean of {self.task} train values: {np.mean(task_values)}')
+
+        #     max_value = np.max(task_values)
+        #     bins = np.arange(0, max_value + 5, 5)
+        #     counts, bin_edges = np.histogram(task_values, bins=bins)
+        #     percentages = counts / counts.sum() * 100
+        #     plt.bar(bin_edges[:-1], percentages, width=np.diff(bin_edges), align='edge', edgecolor='black')
+        #     task_name = self.task.replace("_", " ").capitalize()
+        #     plt.xlabel(f'{task_name} value')
+        #     plt.ylabel('Percentage (%)')
+        #     plt.title(f'{task_name}: Distribution of Task Values in {stage.capitalize()} Dataset')
+        #     plt.tight_layout()
+        #     plt.savefig(f'{self.task}/figures/{self.task}_{self.split_type}_{stage}.png', dpi=300)
+        #     plt.close()
+
+        fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 12), sharex=True) # 3 rows, 1 column, shared x-axis
+
+        for i, stage in enumerate(['train', 'val', 'test']):
+            dataset = getattr(self, f'{stage}_dataset')
+            task_values = [task_value.squeeze() for _, task_value in dataset]
+
+            if stage == 'train':
+                print(f'Mean of {self.task} train values: {np.mean(task_values)}')
+
+            max_value = np.max(task_values)
+            bins = np.arange(0, max_value + 5, 5)
+            counts, bin_edges = np.histogram(task_values, bins=bins)
+            percentages = counts / counts.sum() * 100
+
+            axes[i].bar(bin_edges[:-1], percentages, width=np.diff(bin_edges), align='edge', edgecolor='black')
+            axes[i].set_ylabel('Percentage (%)')
+            axes[i].set_title(stage.capitalize())
+
+        fig.suptitle(self.task.replace("_", " ").capitalize(), fontweight='bold')
+        axes[-1].set_xlabel(f'{self.task.replace("_", " ").capitalize()} value') # sets common x-label
         plt.tight_layout()
-        plt.savefig(f'{self.task}_{stage}.png', dpi=300)
+        plt.savefig(f'{self.task}/figures/{self.task}_{self.split_type}_distributions.png', dpi=300)
         plt.close()
