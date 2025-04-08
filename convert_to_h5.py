18af7700dfb5e8b9cbfcc4ad599d26bd7543eea8
Lucia Gordon <luciagordon@g.harvard.edu>
2025-04-07 22:28:18 -0400

    Added compression

--- convert_to_h5.py
+++ convert_to_h5.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import rasterio
 import subprocess
+import time
 import utils
 
 # ============================================== GLOBAL VARIABLES ============================================== #
@@ -37,10 +38,11 @@ def check_is_number(value):
         return False
 
 def convert_tiffs_to_h5(task):
+    start_time = time.time()
     task_data_dir = f'{data_dir_path}/{task}/data'
     data = defaultdict(list) # dictionary whose default value is an empty list
 
-    with h5py.File(f'{data_dir_path}/{task}/{task}_h5.hdf5', 'w') as h5_file:
+    with h5py.File(f'{data_dir_path}/{task}/{task}.h5', 'w') as h5_file:
         for tiff in os.listdir(task_data_dir):
             tile_id = tiff.split('_')[1]
 
@@ -85,14 +87,16 @@ def convert_tiffs_to_h5(task):
         for key, value in data.items():
             print(key, np.array(value).shape)
 
-            h5_file.create_dataset(key, data=np.array(value))
+            h5_file.create_dataset(key, data=np.array(value), compression='gzip', compression_opts=9)
+
+    print(f'Time taken: {utils.format_time(seconds=time.time()-start_time)}')
 
 if __name__ == '__main__':
     if 'for' not in argv[1]: # python convert_to_h5.py TASK
         partitions = utils.read_yaml('config-user.yml')['partitions'] # list of partition(s)
         env_path = utils.read_yaml('config-user.yml')['env_path'] # path to conda environment
         mem = 300 if argv[1] == 'biomass' else 60
-        subprocess.run(['sbatch', '-t', '0-01:00:00', '-p', partitions, '--mem', f'{mem}G', '--job-name', f'{argv[1]}_convert_to_h5', '-o', f'bash-outputs/{argv[1]}_convert_to_h5.out', '-e', f'bash-errors/{argv[1]}_convert_to_h5.err', 'job.sh', env_path, 'convert_to_h5.py', f'for_{argv[1]}'])
+        subprocess.run(['sbatch', '-t', '15:00:00', '-p', partitions, '--mem', f'{mem}G', '--job-name', f'{argv[1]}_convert_to_h5', '-o', f'bash-outputs/{argv[1]}_convert_to_h5.out', '-e', f'bash-errors/{argv[1]}_convert_to_h5.err', 'job.sh', env_path, 'convert_to_h5.py', f'for_{argv[1]}'])
     else: # python generate_map_data.py for_TASK
         task = argv[1].split('for_')[1]
         print(f'Task = {task}')
