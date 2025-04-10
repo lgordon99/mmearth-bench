35348961938feb5b378204a6d2e2f769225ba4f3
Lucia Gordon <luciagordon@g.harvard.edu>
2025-04-10 12:04:20 -0400

    Added compatibility with sweeps

--- train.py
+++ train.py
@@ -1,8 +1,19 @@
 # ============================================== IMPORTS ============================================== #
 
 from lightning.pytorch import seed_everything
+from omegaconf import DictConfig, OmegaConf
 import hydra
+import os
+import subprocess
+import time
 import torch
+import uuid
+import wandb
+
+# ============================================== GLOBAL ============================================== #
+
+os.environ['JOB_ID'] = str(uuid.uuid4()).split('-')[0]
+OmegaConf.register_new_resolver('env', lambda key: os.environ.get(key, ''))
 
 # ============================================== FUNCTIONS ============================================== #
 
@@ -11,18 +22,35 @@ def train(cfg):
     print(f'Task: {cfg.task}')
     print(f'Split type: {cfg.split_type}')
     print(f'Model type: {cfg.model_type}')
+    # print('Copying h5 file to scratch')
+    # subprocess.run(['rsync', '-ahP', f'{cfg.project_dir_path}/{cfg.task}/{cfg.task}_h5.hdf5', '/scratch'])
+    # print('Copied h5 file to scratch')
 
     torch.set_float32_matmul_precision('high')
     seed_everything(cfg.seed, workers=True)
-
     logger = hydra.utils.instantiate(cfg.logger)
+    _ = logger.experiment # initializes the logger
+    sweep_overridden_parameters = wandb.config
+    print(f'Sweep overridden parameters: {sweep_overridden_parameters}')
+
+    for parameter, value in sweep_overridden_parameters.items():
+        key_1, key_2 = parameter.split('.')
+
+        cfg[key_1][key_2] = value
+
+    print(f'Config: {cfg}')
+
     trainer = hydra.utils.instantiate(cfg.trainer)
     torch.use_deterministic_algorithms(True, warn_only=True)
     datamodule = hydra.utils.instantiate(cfg.datamodule)
+    num_train_batches = len(datamodule.train_dataloader())
+    print(f'Number of training batches: {num_train_batches}')
+    cfg['model']['num_train_batches'] = num_train_batches
+    wandb.config.update(OmegaConf.to_container(cfg, resolve=True))
     model = hydra.utils.instantiate(cfg.model)
 
     trainer.fit(model, datamodule=datamodule)
-    trainer.test(model, datamodule=datamodule)
+    trainer.test(datamodule=datamodule)
 
 if __name__ == '__main__':
     train()
