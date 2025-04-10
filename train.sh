66e8df73f407a6f997c9c0b0219e15a606990d3c
Lucia Gordon <luciagordon@g.harvard.edu>
2025-04-10 12:04:48 -0400

    Gave temp job file a random name

--- train.sh
+++ train.sh
@@ -3,19 +3,24 @@
 TASK=${1}
 SPLIT_TYPE=${2}
 MODEL_TYPE=${3}
+ADAPTATION_MODE=${4}
 
 if [ "$TASK" == "biomass" ]; then
     MEM="60G"
+elif [ "$TASK" == "species" ]; then
+    MEM="30G"
 else
     MEM="20G"
 fi
 
-cat > temp_job.sh <<EOF
+temp_job_file=$(mktemp temp_job_XXXXXX.sh)
+
+cat > $temp_job_file <<EOF
 #!/bin/bash
 #SBATCH --job-name train_${TASK}_${SPLIT_TYPE}_${MODEL_TYPE}
 #SBATCH --time 1-00:00
 #SBATCH --partition davies_gpu,gpu,seas_gpu
-#SBATCH --mem ${MEM}
+#SBATCH --mem $MEM
 #SBATCH --gres gpu:1
 #SBATCH --output bash-outputs/train_${TASK}_${SPLIT_TYPE}_${MODEL_TYPE}.out
 
@@ -24,20 +29,9 @@ conda activate /n/davies_lab/Users/luciagordon/mmearth-bench/mmearth-bench-env
 echo "Task: ${TASK}"
 NUM_GPUS=\$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 echo "Number of GPUs: \$NUM_GPUS"
-SCRATCH_DIR=/scratch
-echo "Copying h5 file to \$SCRATCH_DIR"
-H5_FILE=/n/davies_lab/Users/luciagordon/mmearth-bench/${TASK}/${TASK}_h5.hdf5
-rsync -ahP \$H5_FILE \$SCRATCH_DIR
-echo "Copied h5 file to \$SCRATCH_DIR"
-SPLIT_DATA=/n/davies_lab/Users/luciagordon/mmearth-bench/${TASK}/${TASK}_${SPLIT_TYPE}_split_data.json
-if [ -f "${SPLIT_DATA}" ]; then
-    echo "Copying split data to \$SCRATCH_DIR"
-    rsync -ahP \$SPLIT_DATA \$SCRATCH_DIR
-    echo "Copied split data to \$SCRATCH_DIR"
-fi
-python train.py +task=${TASK} +split_type=${SPLIT_TYPE} +model_type=${MODEL_TYPE}
+python train.py +task=${TASK} +split_type=${SPLIT_TYPE} +model_type=${MODEL_TYPE} +adaptation_mode=${ADAPTATION_MODE}
 
 EOF
 
-sbatch temp_job.sh
-rm temp_job.sh
+sbatch $temp_job_file
+rm $temp_job_file
