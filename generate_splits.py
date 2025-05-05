7551768506af93dd2cff7af6b17cf46b09ba4fb4
Lucia Gordon <luciagordon@g.harvard.edu>
2025-05-05 01:51:39 -0400

    Calculated RMSE using train mean as prediction

--- generate_splits.py
+++ generate_splits.py
@@ -6,6 +6,7 @@ from shapely.geometry import box, Polygon, MultiPolygon
 import geopandas as gpd
 import h5py
 import json
+import numpy as np
 import random
 import utils
 
@@ -15,6 +16,7 @@ training_fraction = 0.7
 validation_fraction = 0.15
 tolerance = 0.1
 height, width = 128, 128
+nodata_value = -9999
 data_dir_path = '/n/davies_lab/Users/luciagordon/mmearth-bench'
 random.seed(42) # for reproducibility
 
@@ -58,18 +60,11 @@ def get_split_data(task):
         crs = h5_file['crs'][:].astype(str).tolist() # coordinate reference system for each tile
         transforms = h5_file['transform'][:] # affine transformation for each tile
         tile_count = len(sentinel2) # number of tiles for the task
-        split_data_path = f'{data_dir_path}/{task}/{task}_split_data_new.json'
-
-        print(f'{task} tile count: {tile_count}')
-        print(f'Sentinel-2: {sentinel2.shape}')
-        print(f'{task}: {task_data.shape}')
 
     boxes = {i: get_box_wgs_84(transform=transforms[i], width=width, height=height, crs=crs[i]) for i in range(tile_count)} # dictionary of boxes for each tile
     africa_boundaries = get_africa_boundaries()
     africa_boxes = {tile_index: box for tile_index, box in boxes.items() if box.intersects(africa_boundaries)} # dictionary of boxes for each tile within the Africa boundaries
     non_africa_boxes = {tile_index: box for tile_index, box in boxes.items() if tile_index not in africa_boxes.keys()} # dictionary of boxes for each tile outside the Africa boundaries
-    print(f'Tiles in Africa: {len(africa_boxes)}')
-    print(f'Tiles outside Africa: {len(non_africa_boxes)}')
 
     # training, validation, and testing tile indices
     non_africa_tile_indices = sorted(list(map(int, non_africa_boxes.keys())))
@@ -85,18 +80,39 @@ def get_split_data(task):
     split_data['random_test_indices'] = sorted(non_africa_tile_indices[end_val_indices:]) # remaining 15% of the non-Africa tiles for testing
     split_data['geographic_test_indices'] = sorted(list(map(int, africa_boxes.keys()))) # Africa tiles for testing
 
-    with open(split_data_path, 'w') as file:
+    with open(f'{data_dir_path}/{task}/{task}_split_data.json', 'w') as file:
         json.dump(split_data, file, indent=4)
-
-    print(f'{len(split_data["train_indices"])} training tiles')
-    print(f'{len(split_data["val_indices"])} validation tiles')
-    print(f'{len(split_data["random_test_indices"])} random test tiles')
-    print(f'{len(split_data["geographic_test_indices"])} geographic test tiles\n')
 
     split_boxes = {split: [boxes[i] for i in split_data[f'{split}_indices']] for split in ['train', 'val', 'random_test', 'geographic_test']}
 
     for split in ['train', 'val', 'random_test', 'geographic_test']:
         gpd.GeoDataFrame(geometry=split_boxes[split], crs='EPSG:4326').to_file(f'{data_dir_path}/{task}/{task}_{split}_tiles.geojson', driver='GeoJSON')
+
+    split_task_values = {split: task_data.squeeze()[split_data[f'{split}_indices']].ravel() for split in ['train', 'val', 'random_test', 'geographic_test']}
+
+    if task == 'biomass':
+        split_task_values = {split: [value for value in split_task_values[split] if value != nodata_value] for split in ['train', 'val', 'random_test', 'geographic_test']}
+
+    train_mean = np.mean(split_task_values['train'])
+    val_rmse = np.sqrt(np.mean((np.array(split_task_values['val']) - train_mean) ** 2))
+    random_test_rmse = np.sqrt(np.mean((np.array(split_task_values['random_test']) - train_mean) ** 2))
+    geographic_test_rmse = np.sqrt(np.mean((np.array(split_task_values['geographic_test']) - train_mean) ** 2))
+
+    with open(f'{data_dir_path}/{task}/{task}_summary.txt', 'w') as txt_file:
+        txt_file.write(f'Task: {task}\n')
+        txt_file.write(f'{task} tile count: {tile_count}\n')
+        txt_file.write(f'Sentinel-2: {sentinel2.shape}\n')
+        txt_file.write(f'{task}: {task_data.shape}\n')
+        txt_file.write(f'Tiles in Africa: {len(africa_boxes)}\n')
+        txt_file.write(f'Tiles outside Africa: {len(non_africa_boxes)}\n')
+        txt_file.write(f'{len(split_data["train_indices"])} training tiles\n')
+        txt_file.write(f'{len(split_data["val_indices"])} validation tiles\n')
+        txt_file.write(f'{len(split_data["random_test_indices"])} random test tiles\n')
+        txt_file.write(f'{len(split_data["geographic_test_indices"])} geographic test tiles\n')
+        txt_file.write(f'Mean of train values: {round(float(train_mean), 2)}\n')
+        txt_file.write(f'Val RMSE using the train mean as the prediction: {round(float(val_rmse), 2)}\n')
+        txt_file.write(f'Random test RMSE using the train mean as the prediction: {round(float(random_test_rmse), 2)}\n')
+        txt_file.write(f'Geographic test RMSE using the train mean as the prediction: {round(float(geographic_test_rmse), 2)}\n')
 
 if __name__ == '__main__':
     for task in ['biomass', 'species', 'soil_nitrogen', 'soil_organic_carbon', 'soil_pH']:
