f6215f395cfc8b05fad670d17331cffcf8119b44
Lucia Gordon <luciagordon@g.harvard.edu>
2025-05-22 08:22:49 -0400

    Removed calculating test RMSE with train mean

--- datamodule.py
+++ datamodule.py
@@ -36,9 +36,6 @@ class DataModule(LightningDataModule):
         for split in splits:
             setattr(self, f'{split}_dataset', Subset(dataset=self.dataset, indices=self.dataset.split_data[f'{split}_indices']))
 
-        if self.task != 'species' and stage == 'test':
-            self._calculate_test_rmse_with_train_mean()
-
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=True, generator=torch.Generator().manual_seed(self.seed))
 
@@ -50,40 +47,3 @@ class DataModule(LightningDataModule):
         geographic_test_dataloader = DataLoader(self.geographic_test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True)
 
         return [random_test_dataloader, geographic_test_dataloader]
-
-    def _calculate_test_rmse_with_train_mean(self):
-        train_values = np.array([task_value.squeeze() for _, task_value in self.train_dataset]).ravel()
-
-        if self.task == 'biomass':
-            train_values = [value for value in train_values if value != -9999]
-
-        train_mean = np.mean(train_values)
-
-        print(f'Mean of train values: {round(float(train_mean), 2)}')
-
-        val_values = np.array([task_value.squeeze() for _, task_value in self.val_dataset]).ravel()
-
-        if self.task == 'biomass':
-            val_values = [value for value in val_values if value != -9999]
-
-        val_rmse = np.sqrt(np.mean((np.array(val_values) - train_mean) ** 2))
-
-        print(f'Val RMSE using the train mean as the prediction: {round(float(val_rmse), 2)}')
-
-        random_test_values = np.array([task_value.squeeze() for _, task_value in self.random_test_dataset]).ravel()
-
-        if self.task == 'biomass':
-            random_test_values = [value for value in random_test_values if value != -9999]
-
-        random_test_rmse = np.sqrt(np.mean((np.array(random_test_values) - train_mean) ** 2))
-
-        print(f'Random test RMSE using the train mean as the prediction: {round(float(random_test_rmse), 2)}')
-
-        geographic_test_values = np.array([task_value.squeeze() for _, task_value in self.geographic_test_dataset]).ravel()
-
-        if self.task == 'biomass':
-            geographic_test_values = [value for value in geographic_test_values if value != -9999]
-
-        geographic_test_rmse = np.sqrt(np.mean((np.array(geographic_test_values) - train_mean) ** 2))
-
-        print(f'Geographic test RMSE using the train mean as the prediction: {round(float(geographic_test_rmse), 2)}')
