eb5798969ea4e673f25fc0d0a802b4714d99eba9
Lucia Gordon <luciagordon@g.harvard.edu>
2025-08-08 11:55:31 -0400

    Normalized the needed modalities

--- dataset.py
+++ dataset.py
@@ -6,7 +6,6 @@ dataset.py
 
 from torch.utils.data import Dataset
 import h5py
-import json
 import numpy as np
 import torch
 import utils
@@ -17,71 +16,61 @@ class MMEarthBenchDataset(Dataset):
     def __init__(self, task, data_dir_path):
         self.task = task
         self.data_dir_path = data_dir_path
+        self.no_data_values = utils.read_json(f'{data_dir_path}/no_data_values.json')
 
-        with h5py.File(f'{data_dir_path}/{task}/{task}.h5', 'r') as h5_file:
-            self.sentinel2 = h5_file['Sentinel2'][:]
-            self.sentinel1 = h5_file['Sentinel1'][:]
-            self.asterdem = h5_file['AsterDEM'][:]
-            self.ethgch = h5_file['ETHGCH'][:]
-            self.dynamicworld = h5_file['DynamicWorld'][:]
-            self.esaworldcover = h5_file['ESA_Worldcover'][:]
-            self.precipitation = h5_file['climate'][:, :3] # first 9 bands are precipitation
-            self.temperature = h5_file['climate'][:, 3:] # last 3 bands are temperature
-            self.geolocation = np.concatenate([h5_file['longitude'][:], h5_file['latitude'][:]], axis=1)
-            self.month = h5_file['month'][:]
-            self.biome = h5_file['biome'][:]
-            self.ecoregion = h5_file['ecoregion'][:]
-            self.task_data = h5_file[task][:]
-            self.tile_count = len(self.sentinel2) # number of tiles for the task
+        if task == 'pretraining':
+            h5_path = '/n/gajos_lab/Lab/luciagordon/MMEarth-train/data_1M_v001_64/data_1M_v001_64.h5'
 
-            print(f'{task} tile count: {self.tile_count}')
-            print(f'Sentinel-2: {self.sentinel2.shape}')
-            print(f'Sentinel-1: {self.sentinel1.shape}')
-            print(f'AsterDEM: {self.asterdem.shape}')
-            print(f'ETHGCH: {self.ethgch.shape}')
-            print(f'DynamicWorld: {self.dynamicworld.shape}')
-            print(f'ESA_Worldcover: {self.esaworldcover.shape}')
-            print(f'Precipitation: {self.precipitation.shape}')
-            print(f'Temperature: {self.temperature.shape}')
-            print(f'Geolocation: {self.geolocation.shape}')
-            print(f'Month: {self.month.shape}')
-            print(f'Biome: {self.biome.shape}')
-            print(f'Ecoregion: {self.ecoregion.shape}')
+            with h5py.File(h5_path, 'r') as h5_file:
+                print(h5_file.keys())
+                self.sentinel2 = h5_file['sentinel2'][:]
+                print(f'Sentinel-2: {self.sentinel2.shape}')
+        else:
+            h5_path = f'{data_dir_path}/{task}/{task}.h5'
+            self.split_data = utils.read_json(f'{data_dir_path}/{task}/{task}_split_data.json')
+
+        with h5py.File(h5_path, 'r') as h5_file:
+            self.modality_data = {modality: h5_file[modality][:] for modality in self.no_data_values.keys()}
+
+            if task != 'pretraining':
+                self.task_data = h5_file[task][:]
+
+        # # convert categorical modalities to one-hot encoding
+        # self.modality_data['DynamicWorld'] = np.eye(self.no_data_values['DynamicWorld']+1)[self.modality_data['DynamicWorld'].astype(int).squeeze(1)].transpose(0, 3, 1, 2)
+        # self.modality_data['ESA_WorldCover'] = np.eye(self.no_data_values['ESA_WorldCover']+1)[self.modality_data['ESA_WorldCover'].astype(int).squeeze(1)].transpose(0, 3, 1, 2)
+        # self.modality_data['biome'] = np.eye(self.no_data_values['biome']+1)[self.modality_data['biome'].astype(int)]
+        # self.modality_data['ecoregion'] = np.eye(self.no_data_values['ecoregion']+1)[self.modality_data['ecoregion'].astype(int)]
+
+        self.tile_count = len(self.modality_data['Sentinel2']) # number of tiles for the task
+
+        print(f'{task} tile count: {self.tile_count}')
+
+        if hasattr(self, 'task_data'):
             print(f'{task}: {self.task_data.shape}')
 
-        self.split_data = utils.read_json(f'{data_dir_path}/{task}/{task}_split_data.json')
-
-        # FIX
-        # if task == 'species':
-        #     train_species_counts = task_data[train_indices].sum(axis=0)
-        #     species_with_too_few_train_observations = np.where(train_species_counts < 50)[0]
-        #     task_data[np.ix_(test_indices, species_with_too_few_train_observations)] = 0
+        for modality, data in self.modality_data.items():
+            print(f'{modality}: {data.shape}')
 
     def __len__(self):
         return self.tile_count
 
     def __getitem__(self, index):
         # modalities for the tile
-        modality_data = {'sentinel2': self.sentinel2[index],
-                         'sentinel1': self.sentinel1[index],
-                         'asterdem': self.asterdem[index],
-                         'ethgch': self.ethgch[index],
-                         'dynamicworld': self.dynamicworld[index],
-                         'esaworldcover': self.esaworldcover[index],
-                         'precipitation': self.precipitation[index],
-                         'temperature': self.temperature[index],
-                         'geolocation': self.geolocation[index],
-                         'month': self.month[index],
-                         'biome': self.biome[index],
-                         'ecoregion': self.ecoregion[index]}
+        tile_modality_data = {modality: data[index] for modality, data in self.modality_data.items()}
 
         # normalization
-        modality_data['sentinel2'] = (modality_data['sentinel2'] - self.split_data['train_band_means']) / self.split_data['train_band_stds'] # normalization
+        for modality in ['Sentinel2', 'Sentinel1', 'AsterDEM', 'ETH_GCH', 'precipitation', 'temperature']:
+            masked = np.ma.masked_equal(tile_modality_data[modality], self.no_data_values[modality]) # masks the no-data values
+            normalized = (masked - self.split_data[f'{modality}_train_means']) / self.split_data[f'{modality}_train_stds'] # normalization
+            tile_modality_data[modality] = normalized.filled(0) # replaces NaNs with the post-normalization mean
 
         # task data for the tile
-        task_data = self.task_data[index]
+        if hasattr(self, 'task_data'):
+            task_data = self.task_data[index]
 
-        if len(task_data.shape) == 2: # for biomass
-            task_data = np.expand_dims(task_data, axis=0)
+            if len(task_data.shape) == 2: # for biomass
+                task_data = np.expand_dims(task_data, axis=0)
 
-        return {modality: torch.tensor(data, dtype=torch.float32) for modality, data in modality_data.items()}, torch.tensor(task_data, dtype=torch.float32)
+            return {modality: torch.tensor(data, dtype=torch.float32) for modality, data in tile_modality_data.items()}, torch.tensor(task_data, dtype=torch.float32)
+        else:
+            return {modality: torch.tensor(data, dtype=torch.float32) for modality, data in tile_modality_data.items()}, {modality: torch.tensor(data, dtype=torch.float32) for modality, data in tile_modality_data.items()}
