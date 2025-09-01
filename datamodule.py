63d31b4c6b9472326c64a4e73eec34fc302b5b11
luciagordon <luciagordon@g.harvard.edu>
2025-09-01 13:25:56 -0400

    Printed dataset length

--- datamodule.py
+++ datamodule.py
@@ -7,34 +7,33 @@ datamodule.py
 from dataset import MMEarthBenchDataset
 from lightning.pytorch import LightningDataModule
 from torch.utils.data import DataLoader, Subset
-import numpy as np
 import torch
 
 # ============================================== CLASSES ============================================== #
 
 class DataModule(LightningDataModule):
-    def __init__(self, task, batch_size, data_dir_path, num_workers, seed):
+    def __init__(self, task, batch_size, num_workers, seed):
         super().__init__()
 
         self.task = task
         self.batch_size = batch_size
-        self.data_dir_path = data_dir_path
         self.num_workers = num_workers
         self.seed = seed
-        self.dataset = MMEarthBenchDataset(task=task, data_dir_path=data_dir_path)
+        self.dataset = MMEarthBenchDataset(task=task)
 
         self.setup('init')
 
     def setup(self, stage):
         if stage == 'init':
             splits = ['train']
-        if stage == 'fit':
+        elif stage == 'fit':
             splits = ['val']
         elif stage == 'test':
             splits = ['random_test', 'geographic_test']
 
         for split in splits:
             setattr(self, f'{split}_dataset', Subset(dataset=self.dataset, indices=self.dataset.split_data[f'{split}_indices']))
+            print(f'{split.capitalize().replace("_", " ")} dataset size: {len(getattr(self, f"{split}_dataset"))}')
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=True, generator=torch.Generator().manual_seed(self.seed))
