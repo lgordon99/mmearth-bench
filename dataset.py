c1cb8eafe7883f0287e00936695496f1a9dc6563
luciagordon <luciagordon@g.harvard.edu>
2025-10-19 16:43:10 -0400

    Incorporated train % and ConvNeXtV2A

--- dataset.py
+++ dataset.py
@@ -64,15 +64,18 @@ def make_anysat_s1(sentinel1):
 # ============================================== CLASSES ============================================== #
 
 class MMEarthBenchDataset(Dataset):
-    def __init__(self, task, architecture, adaptation_mode):
+    def __init__(self, task, architecture, adaptation_mode, train_percent):
         self.task = task
         self.architecture = architecture
         self.adaptation_mode = adaptation_mode
+        self.train_percent = train_percent
         self.split_data = utils.read_json(f'{data_dir_path}/{task}/{task}_split_data.json')
 
         with h5py.File(f'{data_dir_path}/{task}/{task}.h5', 'r') as h5_file:
-            self.input_data = {modality: ensure_2d(h5_file[modality][:]) for modality in normalization_data[self.architecture].keys() if modality in h5_file.keys()}
             self.task_modality_data = {modality: h5_file[modality][:] for modality in task_modalities}
+
+            if architecture != 'ConvNeXtV2A':
+                self.input_data = {modality: ensure_2d(h5_file[modality][:]) for modality in normalization_data[self.architecture].keys() if modality in h5_file.keys()}
 
             if architecture == 'TerraMind':
                 self.input_data['RGB'] = np.ma.stack([make_terramind_rgb(np.ma.masked_equal(sentinel2[[3,2,1]], no_data_values['Sentinel2'])) for sentinel2 in self.input_data['Sentinel2']])
@@ -99,9 +102,10 @@ class MMEarthBenchDataset(Dataset):
         print(f'{task} tile count: {self.tile_count}')
         print(f'{task}: {self.task_data.shape}')
 
-        print('Input data')
-        for modality, data in self.input_data.items():
-            print(f'{modality}: {data.shape}')
+        if hasattr(self, 'input_data'):
+            print('Input data')
+            for modality, data in self.input_data.items():
+                print(f'{modality}: {data.shape}')
 
         print('Task modality data')
         for modality, data in self.task_modality_data.items():
@@ -111,35 +115,6 @@ class MMEarthBenchDataset(Dataset):
         return self.tile_count
 
     def __getitem__(self, index):
-        # input data for the tile
-        tile_input_data = {modality: data[index] for modality, data in self.input_data.items()}
-        architecture_normalization_data = normalization_data[self.architecture]
-        tile_input_data = {modality: data[architecture_normalization_data[modality]['bands']] for modality, data in tile_input_data.items()}
-
-        for modality in tile_input_data.keys():
-            if len(architecture_normalization_data[modality]['means']) > 0: # if the modality has normalization data
-                masked = np.ma.masked_equal(tile_input_data[modality], no_data_values[modality]) if modality in task_modalities else tile_input_data[modality]
-                collapsed_shape = (masked.shape[0],) + (1,) * (masked.ndim - 1) # singleton dimensions for the number of spatial dimensions
-
-                # min-max normalization
-                if self.architecture == 'ScaleMAE' or 'DINO' in self.architecture:
-                    min_value = masked.min(axis=axes_to_collapse).reshape(collapsed_shape)
-                    max_value = masked.max(axis=axes_to_collapse).reshape(collapsed_shape)
-                    masked = (masked - min_value) / (max_value - min_value)
-
-                # extract the VV and VH bands from either the ascending or descending pass, whichever has more valid pixels
-                if modality == 'Sentinel1':
-                    masked = get_vv_vh_least_nans(masked)
-
-                    if self.architecture == 'AnySat':
-                        masked = make_anysat_s1(masked)
-
-                # mean-std normalization
-                normalized = (masked - np.expand_dims(architecture_normalization_data[modality]['means'], axis=axes_to_collapse)) / np.expand_dims(architecture_normalization_data[modality]['stds'], axis=axes_to_collapse)
-                tile_input_data[modality] = normalized.filled(0)
-
-            tile_input_data[modality] = torch.tensor(tile_input_data[modality], dtype=torch.float32) # converts to tensor
-
         # modalities for the tile
         tile_task_modality_data = {modality: {'data': data[index]} for modality, data in self.task_modality_data.items()}
 
@@ -151,7 +126,7 @@ class MMEarthBenchDataset(Dataset):
             # normalization
             if modality in ['Sentinel2', 'Sentinel1', 'AsterDEM', 'ETH_GCH', 'precipitation', 'temperature']: # continuous modalities without an encoding
                 masked = np.ma.masked_equal(tile_task_modality_data[modality]['data'], no_data_values[modality]) # masks the no-data values
-                normalized = (masked - self.split_data[f'{modality}_train_means']) / self.split_data[f'{modality}_train_stds'] # normalization
+                normalized = (masked - self.split_data[f'{modality}_train_{self.train_percent}%_means']) / self.split_data[f'{modality}_train_{self.train_percent}%_stds'] # normalization
                 tile_task_modality_data[modality]['data'] = normalized.filled(0) # replaces NaNs with the post-normalization mean
 
         if self.adaptation_mode in ['multimodal', 'multimodal_joint_training', 'ttt-mjt', 'multimodal_mt3', 'multimodal_sln', 'maml_encode']:
@@ -175,6 +150,38 @@ class MMEarthBenchDataset(Dataset):
         # convert to tensors
         for modality in tile_task_modality_data.keys():
             tile_task_modality_data[modality]['data'] = torch.tensor(tile_task_modality_data[modality]['data'], dtype=torch.float32) # converts to tensor
+
+        # input data for the tile
+        if self.architecture == 'ConvNeXtV2A':
+            tile_input_data = {'RGB': tile_task_modality_data['Sentinel2']['data'][[3, 2, 1]]}
+        else:
+            tile_input_data = {modality: data[index] for modality, data in self.input_data.items()}
+            architecture_normalization_data = normalization_data[self.architecture]
+            tile_input_data = {modality: data[architecture_normalization_data[modality]['bands']] for modality, data in tile_input_data.items()}
+
+            for modality in tile_input_data.keys():
+                if len(architecture_normalization_data[modality]['means']) > 0: # if the modality has normalization data
+                    masked = np.ma.masked_equal(tile_input_data[modality], no_data_values[modality]) if modality in task_modalities else tile_input_data[modality]
+                    collapsed_shape = (masked.shape[0],) + (1,) * (masked.ndim - 1) # singleton dimensions for the number of spatial dimensions
+
+                    # min-max normalization
+                    if self.architecture == 'ScaleMAE' or 'DINO' in self.architecture:
+                        min_value = masked.min(axis=axes_to_collapse).reshape(collapsed_shape)
+                        max_value = masked.max(axis=axes_to_collapse).reshape(collapsed_shape)
+                        masked = (masked - min_value) / (max_value - min_value)
+
+                    # extract the VV and VH bands from either the ascending or descending pass, whichever has more valid pixels
+                    if modality == 'Sentinel1':
+                        masked = get_vv_vh_least_nans(masked)
+
+                        if self.architecture == 'AnySat':
+                            masked = make_anysat_s1(masked)
+
+                    # mean-std normalization
+                    normalized = (masked - np.expand_dims(architecture_normalization_data[modality]['means'], axis=axes_to_collapse)) / np.expand_dims(architecture_normalization_data[modality]['stds'], axis=axes_to_collapse)
+                    tile_input_data[modality] = normalized.filled(0)
+
+                tile_input_data[modality] = torch.tensor(tile_input_data[modality], dtype=torch.float32) # converts to tensor
 
         # task data for the tile
         task_data = self.task_data[index]
