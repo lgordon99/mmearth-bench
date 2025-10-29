de9a26d8a46508a6035025351118a82b09f16511
luciagordon <luciagordon@g.harvard.edu>
2025-10-29 01:52:06 -0400

    Worked on MT3 batching

--- datamodule.py
+++ datamodule.py
@@ -6,121 +6,121 @@ datamodule.py
 
 from dataset import MMEarthBenchDataset
 from lightning.pytorch import LightningDataModule
-from torch.utils.data import DataLoader, Sampler, Subset
+from torch.utils.data import DataLoader, Dataset, Sampler, Subset
 import numpy as np
 import torch
 
 # ============================================== CLASSES ============================================== #
 
-class MT3Sampler(Sampler):
-    def __init__(self, dataset, generator):
-        self.dataset = dataset
-        self.generator = generator
-        self.mini_batch_size = 16
-
-        self.train_indices = np.array(self.dataset.split_data[f'train_100%_indices'])
-        # coordinates = dataset.geolocation[train_indices]
-        # points = np.column_stack((train_indices, coordinates))
-        # self.mini_batches = []
-        # mini_batch_id = 0
-
-        # def partition_recursive(points_subset, bounds):
-        #     """
-        #     Recursively partition points into balanced groups.
-        #     bounds: [min_x, max_x, min_y, max_y]
-        #     """
-        #     nonlocal mini_batch_id # allows mini_batch_id to be accessed and modified in the outer scope
-        #     n = len(points_subset)
-
-        #     # Base case: if points fit in one group (within tolerance)
-        #     if n <= self.mini_batch_size * 1.2:  # allows 20% tolerance
-        #         self.mini_batches.append(points_subset[:, 0]) # adds points to cluster
-        #         print(f'Created mini-batch {mini_batch_id} with {n} samples.')
-        #         mini_batch_id += 1 # increments cluster id
-        #         return # stops recursion
-
-        #     needed_groups = max(1, (n + self.mini_batch_size - 1) // self.mini_batch_size) # calculates how many groups we need
-
-        #     # Decide split direction based on aspect ratio
-        #     min_x, max_x, min_y, max_y = bounds
-        #     width = max_x - min_x
-        #     height = max_y - min_y
-
-        #     # Split along longer dimension
-        #     if width > height:
-        #         axis = 1 # splits on x
-        #     else:
-        #         axis = 2  # splits on y
-
-        #     # Sort points along chosen axis
-        #     sorted_indices = np.argsort(points_subset[:, axis])
-        #     sorted_points = points_subset[sorted_indices]
-
-        #     # Calculate optimal split point
-        #     left_groups = needed_groups // 2
-        #     split_idx = min(left_groups * self.mini_batch_size, n - self.mini_batch_size)
-        #     split_idx = max(self.mini_batch_size, split_idx)
-
-        #     # Find the actual split value (between split_idx-1 and split_idx)
-        #     split_value = (sorted_points[split_idx - 1, axis] + sorted_points[split_idx, axis]) / 2
-
-        #     # Split the points
-        #     left_points = sorted_points[:split_idx]
-        #     right_points = sorted_points[split_idx:]
-
-        #     # Create new bounds for each partition
-        #     if axis == 1:  # Split on x
-        #         left_bounds = [min_x, split_value, min_y, max_y]
-        #         right_bounds = [split_value, max_x, min_y, max_y]
-        #     else:  # Split on y
-        #         left_bounds = [min_x, max_x, min_y, split_value]
-        #         right_bounds = [min_x, max_x, split_value, max_y]
-
-        #     # Recursively partition each half
-        #     partition_recursive(left_points, left_bounds)
-        #     partition_recursive(right_points, right_bounds)
-
-        # # get initial bounds of the points
-        # min_x, min_y = points[:, 1:].min(axis=0)
-        # max_x, max_y = points[:, 1:].max(axis=0)
-
-        # # Add small padding
-        # padding_x = (max_x - min_x) * 0.05
-        # padding_y = (max_y - min_y) * 0.05
-
-        # initial_bounds = [min_x - padding_x, max_x + padding_x,
-        #                   min_y - padding_y, max_y + padding_y]
-
-        # partition_recursive(points, initial_bounds)
-        # self.length = len(self.mini_batches)
-        self.length = (len(self.train_indices) + self.mini_batch_size - 1) // self.mini_batch_size
-
-    def __iter__(self):
-        # mini_batch_ids = self.mini_batches.keys()
-        # mini_batch_ids = np.array(mini_batch_ids)[torch.randperm(len(mini_batch_ids), generator=self.generator).numpy()]
-
-        # # Shuffle within each mini batch
-        # for i, mini_batch in enumerate(self.mini_batches.items()):
-        #     shuffled_mini_batch_indices = torch.randperm(len(mini_batch), generator=self.generator).numpy()
-        #     mini_batch[i] = [mini_batch[j] for j in shuffled_mini_batch_indices]
-
-        # # Shuffle the order of minibatches
-        # batch_order = torch.randperm(len(self.mini_batches), generator=self.generator).numpy()
-        # shuffled_mini_batches = []
-
-        # for i in batch_order:
-        #     shuffled_mini_batches.append(self.mini_batches[i])
-
-        # assert len(shuffled_mini_batches) == self.__len__()
-        # return iter(shuffled_mini_batches)
-
-        for i in range(0, len(self.train_indices), self.mini_batch_size):
-            mini_batch_indices = self.train_indices[i:i+self.mini_batch_size]
-
-            yield mini_batch_indices
+class WithIndex(Dataset):
+    def __init__(self, base_dataset):
+        self.base = base_dataset
 
     def __len__(self):
-        return self.length
+        return len(self.base)
+
+    def __getitem__(self, idx):
+        sample = self.base[idx]
+
+        return idx, sample
+
+class MT3Sampler(Sampler):
+    def __init__(self, dataset, generator, mini_batch_size):
+        self.dataset = dataset
+        self.generator = generator
+        self.mini_batch_size = mini_batch_size
+        self.batch_size = 4
+        self.indices = np.array(self.dataset.split_data[f'train_100%_indices'])
+        self.index_to_slot = {}
+
+        self._build_spatial_mini_batches()
+
+    def _build_spatial_mini_batches(self):
+        indices = np.array(self.dataset.split_data[f'train_100%_indices'])
+        coordinates = self.dataset.geolocation[indices]
+        points = np.column_stack((indices, coordinates))
+        self.mini_batches = {}
+        mini_batch_id = 0
+
+        def partition_recursive(points_subset, bounds):
+            """
+            Recursively partition points into balanced groups.
+            bounds: [min_x, max_x, min_y, max_y]
+            """
+            nonlocal mini_batch_id # allows batch_id to be accessed and modified in the outer scope
+            n = len(points_subset)
+
+            # Base case: if points fit in one group (within tolerance)
+            if n <= self.mini_batch_size * 1.2:  # allows 20% tolerance
+                self.mini_batches[mini_batch_id] = points_subset # adds points to cluster
+                print(f'Created batch {mini_batch_id} with {n} tiles')
+                mini_batch_id += 1 # increments cluster id
+                return # stops recursion
+
+            needed_groups = max(1, (n + self.mini_batch_size - 1) // self.mini_batch_size) # calculates how many groups we need
+
+            # Decide split direction based on aspect ratio
+            min_x, max_x, min_y, max_y = bounds
+            width = max_x - min_x
+            height = max_y - min_y
+
+            # Split along longer dimension
+            if width > height:
+                axis = 1 # splits on x
+            else:
+                axis = 2  # splits on y
+
+            # Sort points along chosen axis
+            sorted_indices = np.argsort(points_subset[:, axis])
+            sorted_points = points_subset[sorted_indices]
+
+            # Calculate optimal split point
+            left_groups = needed_groups // 2
+            split_idx = min(left_groups * self.mini_batch_size, n - self.mini_batch_size)
+            split_idx = max(self.mini_batch_size, split_idx)
+
+            # Find the actual split value (between split_idx-1 and split_idx)
+            split_value = (sorted_points[split_idx - 1, axis] + sorted_points[split_idx, axis]) / 2
+
+            # Split the points
+            left_points = sorted_points[:split_idx]
+            right_points = sorted_points[split_idx:]
+
+            # Create new bounds for each partition
+            if axis == 1:  # Split on x
+                left_bounds = [min_x, split_value, min_y, max_y]
+                right_bounds = [split_value, max_x, min_y, max_y]
+            else:  # Split on y
+                left_bounds = [min_x, max_x, min_y, split_value]
+                right_bounds = [min_x, max_x, split_value, max_y]
+
+            # Recursively partition each half
+            partition_recursive(left_points, left_bounds)
+            partition_recursive(right_points, right_bounds)
+
+        # get initial bounds of the points
+        min_x, min_y = points[:, 1:].min(axis=0)
+        max_x, max_y = points[:, 1:].max(axis=0)
+
+        # Add small padding
+        padding_x = (max_x - min_x) * 0.05
+        padding_y = (max_y - min_y) * 0.05
+        initial_bounds = [min_x - padding_x, max_x + padding_x,
+                          min_y - padding_y, max_y + padding_y]
+
+        partition_recursive(points, initial_bounds)
+
+    def __iter__(self):
+        mini_batches = {mini_batch_id: mini_batch[:, 0][torch.randperm(len(mini_batch), generator=self.generator).numpy()].tolist() for mini_batch_id, mini_batch in self.mini_batches.items()}
+        mini_batch_ids = list(mini_batches.keys())
+        mini_batch_ids_perm = torch.randperm(len(mini_batch_ids), generator=self.generator).numpy()
+        shuffled_mini_batch_ids = [mini_batch_ids[i] for i in mini_batch_ids_perm]
+        shuffled_mini_batches = {mini_batch_id: mini_batches[mini_batch_id] for mini_batch_id in shuffled_mini_batch_ids}
+
+        return iter([mini_batch for mini_batch in shuffled_mini_batches.values()])
+
+    def __len__(self):
+        return len(self.mini_batches)
 
 class GeographicSampler(Sampler):
     def __init__(self, dataset, batch_size, split):
@@ -246,11 +246,10 @@ class GeographicSampler(Sampler):
         #     json.dump({"type": "FeatureCollection", "features": features}, f, indent=2)
 
         # print(f"GeoJSON saved to: {output_file}")
-
+        # exit()
     def __iter__(self):
         indices = [batch[:, 0] for batch in self.batches.values()]
         return iter([int(idx) for batch in indices for idx in batch])
-        # return iter([int(idx) for batch in self.batches for idx in batch])
 
     def __len__(self):
         return len(self.indices)
@@ -338,19 +337,25 @@ class DataModule(LightningDataModule):
             print(f'{split.capitalize().replace("_", " ")} dataset size: {len(getattr(self, f"{split}_dataset"))}')
 
     def train_dataloader(self):
-        if self.adaptation_mode == 'UDA-SS':
-            balanced_domain_sampler = BalancedDomainSampler(dataset=self.train_dataset, generator=torch.Generator().manual_seed(self.seed))
-            return DataLoader(self.train_dataset, batch_size=self.batch_size, sampler=balanced_domain_sampler, num_workers=self.num_workers, pin_memory=True)
-        # elif self.adaptation_mode == 'MT3_metabatch':
-        #     mt3_sampler = MT3Sampler(dataset=self.dataset, generator=torch.Generator().manual_seed(self.seed))
-        #     return DataLoader(self.train_dataset, batch_size=4, sampler=mt3_sampler, num_workers=self.num_workers, pin_memory=True)
+        # if self.adaptation_mode == 'UDA-SS':
+        #     balanced_domain_sampler = BalancedDomainSampler(dataset=self.train_dataset, generator=torch.Generator().manual_seed(self.seed))
+        #     return DataLoader(self.train_dataset, batch_size=self.batch_size, sampler=balanced_domain_sampler, num_workers=self.num_workers, pin_memory=True)
+        if self.adaptation_mode == 'MT3_metabatch':
+            mt3_sampler = MT3Sampler(dataset=self.dataset, generator=torch.Generator().manual_seed(self.seed), mini_batch_size=round(self.batch_size/4))
+            return DataLoader(self.train_dataset, batch_size=4, sampler=mt3_sampler, num_workers=self.num_workers, pin_memory=True)
         else:
             return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=True, generator=torch.Generator().manual_seed(self.seed))
 
     def val_dataloader(self):
         batch_size = 1 if self.adaptation_mode in ['mt3', 'MT3_metabatch', 'multimodal_mt3', 'sln', 'multimodal_sln', 'maml_input_embeddings', 'maml_encode', 'rna', 'rna_input_embeddings'] else self.batch_size
 
-        return DataLoader(self.val_dataset, batch_size=batch_size, num_workers=self.num_workers, pin_memory=True)
+        if 'TTT-Geo' in self.adaptation_mode:
+            val_geographic_sampler = GeographicSampler(dataset=self.dataset, batch_size=batch_size, split='val')
+            val_dataloader = DataLoader(self.dataset, batch_size=batch_size, sampler=val_geographic_sampler, num_workers=self.num_workers, pin_memory=True)
+
+            return val_dataloader
+        else:
+            return DataLoader(self.val_dataset, batch_size=batch_size, num_workers=self.num_workers, pin_memory=True)
 
     def test_dataloader(self):
         batch_size = 1 if self.adaptation_mode in ['mt3', 'MT3_metabatch', 'multimodal_mt3', 'sln', 'multimodal_sln', 'maml_input_embeddings', 'maml_encode', 'rna', 'rna_input_embeddings'] else self.batch_size
