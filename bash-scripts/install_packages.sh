0dc1dfd51759ac9ae7e6d54d977d83c935049ac5
luciagordon <luciagordon@g.harvard.edu>
2026-01-11 16:12:03 -0500

    Added packages

--- bash-scripts/install_packages.sh
+++ bash-scripts/install_packages.sh
@@ -1,19 +1,6 @@
-conda install python
-conda install pip
-
-pip install beautifulsoup4
-pip install cartopy
-pip install earthengine-api
-pip install h5py
-pip install numpy
-pip install geemap
-pip install geojson
-pip install geopandas
-pip install pandas
-pip install pyproj
-pip install rasterio
-pip install scipy
-pip install wikipedia-api
+conda install -c conda-forge gdal=3.10 proj geos geotiff -y
+pip install -r requirements.txt
+pip install --no-deps "git+https://github.com/IBM/terratorch.git"
 
 earthengine authenticate
 
