3b4b6603ebea4b722a2db2f5601eeec4a8187615
luciagordon <luciagordon@g.harvard.edu>
2026-01-13 16:53:27 -0500

    Parallelized runs

---
+++ run_multi.sh
@@ -0,0 +1,13 @@
+for task in "biomass" "soil_nitrogen" "soil_organic_carbon" "soil_pH" "species"; do
+    for architecture in "ConvNeXtV2A" "ScaleMAE" "DINOv3Web" "DINOv3Sat" "SatlasNet" "MPMAE" "TerraMind" "CopernicusFM" "TerraMindS2" "CopernicusFMS2"; do
+        for train_percent in 5 50 100; do
+            bash run.sh $task $architecture FT $train_percent
+            bash run.sh $task $architecture LP $train_percent
+        done
+    done
+    for architecture in "ConvNeXtV2A" "ScaleMAE" "DINOv3Web" "DINOv3Sat" "SatlasNet" "MPMAE" "TerraMind" "CopernicusFM"; do
+        bash run.sh $task $architecture JT 100
+        bash run.sh $task $architecture JT-TTT 100
+        bash run.sh $task $architecture JT-TTT-Geo 100
+    done
+done
