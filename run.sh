a5d05e5259167e0b3a8a050500571b0bbb4802c4
luciagordon <luciagordon@g.harvard.edu>
2026-03-03 08:33:52 -0500

    Incorporated seed

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ task=${1}
 architecture=${2}
 adaptation_mode=${3}
 train_percent=${4}
+seed=${5}
 
 data_dir_path=$(python -c "import yaml; print(yaml.safe_load(open('config-user.yml'))['data_dir_path'])")
 
@@ -9,8 +10,6 @@ if [[ "$adaptation_mode" == *TTT* ]]; then
     TIME="0-6:00"
 elif [[ "$adaptation_mode" == "FT" || "$adaptation_mode" == "JT" ]]; then
     TIME="1-00:00"
-else
-    TIME="2-00:00"
 fi
 
 if [[ "$task" == "species" ]]; then
@@ -30,18 +29,18 @@ else
     NUM_GPUS=1
 fi
 
-bash_file="${data_dir_path}/experiments/${task}_${architecture}_${adaptation_mode}_${train_percent}_run.sh"
+bash_file="${data_dir_path}/experiments/${task}_${architecture}_${adaptation_mode}_${train_percent}_${seed}_run.sh"
 
 cat > $bash_file <<EOF
 #!/bin/bash
-#SBATCH --job-name ${task}_${architecture}_${adaptation_mode}_${train_percent}
+#SBATCH --job-name ${task}_${architecture}_${adaptation_mode}_${train_percent}_${seed}
 #SBATCH --time $TIME
-#SBATCH --partition seas_gpu,gpu_h200
+#SBATCH --partition seas_gpu
 #SBATCH --mem $MEM
 #SBATCH --ntasks-per-node $NUM_GPUS
 #SBATCH --gres gpu:nvidia_h200:$NUM_GPUS
-#SBATCH --output ${data_dir_path}/experiments/output-files/${task}/${task}_${architecture}_${adaptation_mode}_${train_percent}.out
-#SBATCH --account gajos_lab
+#SBATCH --output ${data_dir_path}/experiments/output-files/${task}/${task}_${architecture}_${adaptation_mode}_${train_percent}_${seed}.out
+#SBATCH --account davies_lab
 
 source ~/.bashrc
 conda activate $data_dir_path/mmearth-bench-env
@@ -51,8 +50,8 @@ echo "Adaptation mode: ${adaptation_mode}"
 echo "Train percent: ${train_percent}"
 NUM_GPUS=\$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 echo "Number of GPUs: \$NUM_GPUS"
-export WANDB_CACHE_DIR=/tmp/${task}_${architecture}_${adaptation_mode}_${train_percent}/wandb_cache
-srun python train.py +task=${task} +architecture=${architecture} +adaptation_mode=${adaptation_mode} +train_percent=${train_percent}
+export WANDB_CACHE_DIR=/tmp/${task}_${architecture}_${adaptation_mode}_${train_percent}_${seed}/wandb_cache
+srun python train.py +task=${task} +architecture=${architecture} +adaptation_mode=${adaptation_mode} +train_percent=${train_percent} seed=${seed}
 EOF
 
 sbatch $bash_file
